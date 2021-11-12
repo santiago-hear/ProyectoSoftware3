27cc69bcf0cf2af660ef7e631562232e7c868fca
santiago-hear <santiago.1701423560@ucaldas.edu.co>
2021-11-12 18:31:48 -0500

    Autenticación/login

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,7 @@ Route::prefix('dashboard')->group(function() {
     Route::resource('post', PostController::class);
     Route::resource('category', CategoryController::class);
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
